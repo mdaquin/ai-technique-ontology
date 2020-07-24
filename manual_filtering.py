1a629fb487939e4f861b9e8c727a6f02215cdf95
mdaquin <mathieu@daquin.net>
2020-07-24 23:01:54 +0100

    updated manual annotation and used to seprate topics, techniques and systems in y_entities

--- manual_filtering.py
+++ manual_filtering.py
@@ -4,23 +4,30 @@ import random
 import sys
 import config
 
+if len(sys.argv) != 3:
+    print ("provide a filename and a sentence")
+    sys.exit()
+
+dfile = sys.argv[1]
+sentence = sys.argv[2]
+    
 all_entities = {}
-with open("data/entities.json") as f:
+with open("data/"+dfile+".json") as f:
     all_entities = json.load(f)
 
 y_entities = {}
-if os.path.isfile("data/y_entities.json"):
-    with open("data/y_entities.json") as f:
+if os.path.isfile("data/y_"+dfile+".json"):
+    with open("data/y_"+dfile+".json") as f:
         y_entities = json.load(f)
 
 n_entities = {}
-if os.path.isfile("data/n_entities.json"):
-    with open("data/n_entities.json") as f:
+if os.path.isfile("data/n_"+dfile+".json"):
+    with open("data/n_"+dfile+".json") as f:
         n_entities = json.load(f)    
 
 u_entities = {}
-if os.path.isfile("data/u_entities.json"):
-    with open("data/u_entities.json") as f:
+if os.path.isfile("data/u_"+dfile+".json"):
+    with open("data/u_"+dfile+".json") as f:
         u_entities = json.load(f)    
 
 alreadydone = []
@@ -41,19 +48,19 @@ while len(todo) > 0:
     e = todo.pop(i)
     if "label" in all_entities[e]:
         try:
-            answer = raw_input("Is https://www.wikidata.org/wiki/"+e+" - "+all_entities[e]["label"]+" relevant to "+all_entities[config.eid]["label"]+"?\n y(es) - n(o) - u(nsure) - q(uit)\n")
+            answer = raw_input("Is https://www.wikidata.org/wiki/"+e+" - "+all_entities[e]["label"]+" "+sentence+"?\n y(es) - n(o) - u(nsure) - q(uit)\n")
             if answer == "q": sys.exit()
             if answer == "y":
                 y_entities[e] = all_entities[e]
-                with open('data/y_entities.json', 'w') as outfile:
+                with open('data/y_'+dfile+'.json', 'w') as outfile:
                     json.dump(y_entities, outfile)
             if answer == "n":
                 n_entities[e] = all_entities[e]
-                with open('data/n_entities.json', 'w') as outfile:
+                with open('data/n_'+dfile+'.json', 'w') as outfile:
                     json.dump(n_entities, outfile)
             if answer == "u":
                 u_entities[e] = all_entities[e]
-                with open('data/u_entities.json', 'w') as outfile:
+                with open('data/u_'+dfile+'.json', 'w') as outfile:
                     json.dump(u_entities, outfile)
         except UnicodeEncodeError:
             print("-")
