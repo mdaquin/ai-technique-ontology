0df7780a3e47e775f6d0a90229577900b0aac851
mdaquin <mathieu@daquin.net>
2020-07-26 20:42:09 +0100

    initial annotation

--- attributes.py
+++ attributes.py
@@ -3,7 +3,7 @@
 a = {
     "input_type": ["numeric", "symbolic", "text", "structured", "unstructured", "other", "temporal"],
     "inspiration": ["cognitive", "biological", "abstract", "other"],
-    "background_input_type": ["rules", "topology", "examples", "number", "agents"],
+    "background_input_type": ["rules", "topology", "examples", "number", "agents", "evaluation_function", "other"],
     "output_type": ["number", "label", "text", "facts", "structured", "unstructured", "other"],
     "task": ["inference", "regression", "classification", "learning", "recognition", "extraction", "optimisation", "search", "representation", "other"]
     }
